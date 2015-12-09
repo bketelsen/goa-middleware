f6b037ff2f76709da92e9f6f39a5be510f54c5c9
Brian Ketelsen <bketelsen@gmail.com>
2015-12-08 20:19:39 -0500

    moving TokenManager into context

--- jwt.go
+++ jwt.go
@@ -2,7 +2,6 @@ package middleware
 
 import (
 	"fmt"
-	"io/ioutil"
 	"net/http"
 	"strings"
 
@@ -10,28 +9,56 @@ import (
 	"github.com/raphael/goa"
 )
 
+type SigningMethod int
+
+const (
+	RSA256 SigningMethod = iota
+	RSA384
+	RSA512
+	HMAC256
+	HMAC384
+	HMAC512
+)
+
+var signingmethods map[SigningMethod]string
+
+func init() {
+	signingmethods = make(map[SigningMethod]string)
+
+	signingmethods[RSA256] = "RS256"
+	signingmethods[RSA384] = "RS512"
+	signingmethods[RSA512] = "RS512"
+	signingmethods[HMAC256] = "HS256"
+	signingmethods[HMAC384] = "HS384"
+	signingmethods[HMAC512] = "HS512"
+}
+
 // JWTKey is the JWT middleware key used to store the token in the context.
 const JWTKey middlewareKey = 0
+
+// JWTKey is the JWT middleware key used to store the token in the context.
+const TokenManagerKey middlewareKey = 1
 
 // JWTHeader is the name of the header used to transmit the JWT token.
 const JWTHeader = "Authorization"
 
-// Keyfunc is a function that takes a token and returns the key to validate that
+// ValidationKeyfunc is a function that takes a token and returns the key to validate that
 // token, which allows it to use inforamtion from the key itself to choose the key
 // to return.
-type Keyfunc func(*jwt.Token) (interface{}, error)
+type ValidationKeyfunc func(*jwt.Token) (interface{}, error)
 
-func keyFuncWrapper(k Keyfunc) jwt.Keyfunc {
+func keyFuncWrapper(k ValidationKeyfunc) jwt.Keyfunc {
 	return func(tok *jwt.Token) (interface{}, error) {
 		return k(tok)
 	}
 }
 
-//TEMP
-// Sample data from http://tools.ietf.org/html/draft-jones-json-web-signature-04#appendix-A.1
-var hmacTestKey, _ = ioutil.ReadFile("test/hmacTestKey")
+// KeyFunc is a function that returns the key to sign a
+// token.  It should return a []byte (for HMAC or RSA)
+// or a *rsa.PublicKey (RSA only)
+type KeyFunc func() (interface{}, error)
 
-type JWTSpecification struct {
+type Specification struct {
 	// TokenHeader is the HTTP header to search for the JWT Token
 	// Defaults to "Authorization"
 	TokenHeader string
@@ -44,25 +71,39 @@ type JWTSpecification struct {
 	AllowParam bool
 	// ValidationFunc is a function that returns the key to validate the JWT
 	// Required, no default
-	ValidationFunc Keyfunc
+	ValidationFunc ValidationKeyfunc
 	// AuthOptions is a flag that determines whether a token is required on OPTIONS
 	// requests
 	AuthOptions bool
+	// TTLMinutes is the TTL for tokens that are generated
+	TTLMinutes int
+	// Issuer is the name of the issuer that will be inserted into the
+	// generated token's claims
+	Issuer string
+	// KeySigningMethod determines the type of key that will be used to sign
+	// Tokens.
+	KeySigningMethod SigningMethod
+	// SigningKeyFunc is a function that retu
+	SigningKeyFunc KeyFunc
+	// CommonClaims is a list of claims added to all tokens issued
+	CommonClaims map[string]interface{}
 }
 
 // JWTMiddleware is a middleware that retrieves a JWT token from the request if present and
 // injects it into the context.  It checks for the token in the HTTP Headers first, then the querystring if
 // the specification "AllowParam" is true.
 // Retrieve it using ctx.Value(JWTKey).
-func JWTMiddleware(spec JWTSpecification) goa.Middleware {
+func JWTMiddleware(spec Specification) goa.Middleware {
 	if spec.TokenHeader == "" {
 		spec.TokenHeader = "Authorization"
 	}
 	if spec.TokenParam == "" {
 		spec.TokenParam = "token"
 	}
+	tokenManager := NewTokenManager(spec)
 	return func(h goa.Handler) goa.Handler {
 		return func(ctx *goa.Context) error {
+			ctx.SetValue(TokenManagerKey, tokenManager)
 			// If AuthOptions is false, and this is an OPTIONS request
 			// just let the request fly
 			if !spec.AuthOptions && ctx.Request().Method == "OPTIONS" {
--- token.go
+++ token.go
@@ -6,27 +6,24 @@ import (
 	"github.com/dgrijalva/jwt-go"
 )
 
-type TokenSpecification struct {
-	TTLMinutes     int
-	Issuer         string
-	SigningKeyFunc Keyfunc
-}
-
 type TokenManager struct {
-	spec TokenSpecification
+	spec *Specification
 }
 
 // NewTokenManager returns a TokenManager.  If TTLMinutes isn't specified
 // it will default to 5 minutes.
-func NewTokenManager(spec TokenSpecification) *TokenManager {
+func NewTokenManager(spec *Specification) *TokenManager {
 	if spec.TTLMinutes != 0 {
 		spec.TTLMinutes = 5
 	}
 	return &TokenManager{spec: spec}
 }
+
+// Create makes a new token, adding the claims provided.  It returns
+// a token as a string.
 func (tm *TokenManager) Create(claims map[string]interface{}) (string, error) {
-	// create a signer for hmac256
-	t := jwt.New(jwt.GetSigningMethod("HS256"))
+
+	t := jwt.New(jwt.GetSigningMethod(signingmethods[tm.spec.KeySigningMethod]))
 
 	for k, v := range claims {
 		t.Claims[k] = v
@@ -35,10 +32,14 @@ func (tm *TokenManager) Create(claims map[string]interface{}) (string, error) {
 	if tm.spec.Issuer != "" {
 		t.Claims["iss"] = tm.spec.Issuer
 	}
+
+	for k, v := range tm.spec.CommonClaims {
+		t.Claims[k] = v
+	}
 	// set issued at time
 	t.Claims["iat"] = time.Now().Unix()
 	// set the expire time
 	t.Claims["exp"] = time.Now().Add(time.Minute * time.Duration(tm.spec.TTLMinutes)).Unix()
-	return t.SignedString(keyFuncWrapper(tm.spec.SigningKeyFunc))
+	return t.SignedString(tm.spec.SigningKeyFunc)
 
 }

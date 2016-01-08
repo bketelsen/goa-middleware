25532ffa7291c1d76e42e6c3e567f9efb136daa7
Brian Ketelsen <bketelsen@gmail.com>
2016-01-08 11:27:02 -0500

    remove jwt/osin implementation but add lots of documentation

--- jwt/token.go
+++ jwt/token.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"time"
 
-	"github.com/RangelReale/osin"
 	"github.com/dgrijalva/jwt-go"
 )
 
@@ -57,76 +56,4 @@ func (tm *TokenManager) Create(claims map[string]interface{}) (string, error) {
 	}
 	return t.SignedString(bytes)
 
-}
-
-// GenerateAuthorizeToken satisfies the AuthorizeTokenGen interface for RangelReale/osin
-// So that osin may be used for storage and token generation.  GenerateAuthorizeToken
-// returns an access token
-func (tm *TokenManager) GenerateAuthorizeToken(data *osin.AuthorizeData) (string, error) {
-
-	t := jwt.New(jwt.GetSigningMethod(signingmethods[tm.spec.KeySigningMethod]))
-	if tm.spec.Issuer != "" {
-		t.Claims["iss"] = tm.spec.Issuer
-	}
-
-	for k, v := range tm.spec.CommonClaims {
-		t.Claims[k] = v
-	}
-	t.Claims["scopes"] = data.Scope
-	// set issued at time
-	t.Claims["iat"] = data.CreatedAt.Unix()
-	// set the expire time
-	t.Claims["exp"] = time.Now().Add(time.Second * time.Duration(data.ExpiresIn)).Unix()
-	bytes, err := tm.spec.SigningKeyFunc()
-	if err != nil {
-		return "", fmt.Errorf("Error retrieving Signing Key: %v", err)
-	}
-	return t.SignedString(bytes)
-
-}
-
-// GenerateAccessToken satisfies the AuthorizeTokenGen interface for RangelReale/osin
-// So that osin may be used for storage and token generation.  GenerateAccessToken
-// creates both an access token and a refresh token.
-func (tm *TokenManager) GenerateAccessToken(data *osin.AccessData, generaterefresh bool) (accesstoken string, refreshtoken string, err error) {
-	t := jwt.New(jwt.GetSigningMethod(signingmethods[tm.spec.KeySigningMethod]))
-	if tm.spec.Issuer != "" {
-		t.Claims["iss"] = tm.spec.Issuer
-	}
-
-	for k, v := range tm.spec.CommonClaims {
-		t.Claims[k] = v
-	}
-	t.Claims["scopes"] = data.Scope
-	// set issued at time
-	t.Claims["iat"] = data.CreatedAt.Unix()
-	// set the expire time
-	t.Claims["exp"] = time.Now().Add(time.Second * time.Duration(data.ExpiresIn)).Unix()
-	bytes, err := tm.spec.SigningKeyFunc()
-	if err != nil {
-		return "", "", fmt.Errorf("Error retrieving Signing Key: %v", err)
-	}
-	accesstoken, err = t.SignedString(bytes)
-	if err != nil {
-		return "", "", err
-	}
-	r := jwt.New(jwt.GetSigningMethod(signingmethods[tm.spec.KeySigningMethod]))
-	if tm.spec.Issuer != "" {
-		r.Claims["iss"] = tm.spec.Issuer
-	}
-
-	for k, v := range tm.spec.CommonClaims {
-		r.Claims[k] = v
-	}
-	r.Claims["scopes"] = data.Scope
-	// set issued at time
-	r.Claims["iat"] = data.CreatedAt.Unix()
-	// set the expire time
-	r.Claims["exp"] = time.Now().Add(time.Minute * time.Duration(tm.spec.RefreshTTLMinutes)).Unix()
-	bytes, err = tm.spec.SigningKeyFunc()
-	if err != nil {
-		return "", "", fmt.Errorf("Error retrieving Signing Key: %v", err)
-	}
-	refreshtoken, err = r.SignedString(bytes)
-	return accesstoken, refreshtoken, err
 }
